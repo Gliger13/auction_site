17c815a329eab790761c5f419dc60f5787a4d73a
Gliger13 <zanevskiyandrey@gmail.com>
2020-05-22 21:19:53 +0300

    Update and keep secrets

--- auction/auction/settings.py
+++ auction/auction/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from auction import secrets
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uwvxe%460i$c+2^o%5=_a2yput$quli%f6iva4n67i_x1_ak_@'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
