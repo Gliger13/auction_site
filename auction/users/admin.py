ea099f8bff7c3ee0e3e17f74555bee99a82db0d9
Gliger13 <zanevskiyandrey@gmail.com>
2020-05-25 16:14:04 +0300

    Add users app

---
+++ auction/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import User, Avatar
+
+admin.site.register(User)
+admin.site.register(Avatar)
---
+++ auction/users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    telephone = models.CharField(
+        max_length=10,
+        null=True,
+        unique=True
+    )
+
+    class Meta:
+        ordering = ['username', 'id']
+
+
+class Avatar(models.Model):
+    user = models.ForeignKey(User, related_name='avatar', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        get_latest_by = 'created_at'
+
+    def __str__(self):
+        return f"{self.user.username} avatar"
