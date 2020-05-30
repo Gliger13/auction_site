e2a1f49ce992dcfa76b9f70061649c9c0a6b7321
Gliger13 <zanevskiyandrey@gmail.com>
2020-05-30 18:05:27 +0300

    Lots: connect form and model of Bet

--- auction/lots/forms.py
+++ auction/lots/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from lots.models import Lot
+from lots.models import Lot, Bet
 
 
 class LotsForm(forms.ModelForm):
@@ -32,3 +32,33 @@ class LotsForm(forms.ModelForm):
 
     def save(self, commit=True):
         return super().save(commit)
+
+
+class SetBitForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(SetBitForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'uk-input uk-form-width-medium'
+
+    set_price = forms.IntegerField(
+        widget=forms.NumberInput,
+        required=True
+    )
+
+    class Meta:
+        model = Bet
+        fields = [
+            'set_price'
+        ]
+
+    def is_valid(self):
+        return super(SetBitForm, self).is_valid()
+
+    def clean_lot(self, lot):
+        if (lot.current_price >= int(self.data['set_price']) or
+                lot.base_price >= int(self.data['set_price'])):
+            return False
+        else:
+            lot.current_price = int(self.data['set_price'])
+            lot.save()
+            return True
--- auction/lots/views.py
+++ auction/lots/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
-from lots.forms import LotsForm
-from lots.models import Lot
+from lots.forms import LotsForm, SetBitForm
+from lots.models import Lot, Bet
 
 
+@login_required
 def create(request):
     if request.method == 'GET':
         form = LotsForm()
@@ -32,11 +34,10 @@ def page(request, num):
     if request.method == 'GET':
         lots_list = Lot.objects.all()
 
-        paginator = Paginator(lots_list, 5)
+        paginator = Paginator(lots_list, 5, orphans=2)
 
         page_number = num
         page_obj = paginator.get_page(page_number)
-        print(page_obj)
         return render(
             request,
             'lots/page.html',
@@ -46,13 +47,40 @@ def page(request, num):
         )
 
 
+@login_required
 def lot(request, lot_id):
+    lot = Lot.objects.filter(id=lot_id).get()
     if request.method == 'GET':
-        lot = Lot.objects.filter(id=lot_id).get()
+        form = SetBitForm()
         return render(
             request,
             'lots/lot.html',
             context={
-                'lot': lot
+                'lot': lot,
+                'form': form
             }
         )
+    elif request.method == 'POST':
+        bet = Bet()
+        bet.lot = lot
+        form = SetBitForm(request.POST, instance=bet)
+        if form.is_valid() and form.clean_lot(lot):
+            form.is_correct = True
+            form.save()
+            return render(
+                request,
+                'lots/lot.html',
+                context={
+                    'lot': lot,
+                    'form': form
+                }
+            )
+        else:
+            form.is_correct = False
+            return render(
+                request,
+                'lots/lot.html',
+                context={
+                    'lot': lot,
+                    'form': form
+                })
